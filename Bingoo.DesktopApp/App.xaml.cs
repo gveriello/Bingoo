144f798026769487165ad0333ab9a2324df9b6af
Giuseppe Veriello <Giuseppe.Veriello@eng.it>
2023-12-11 15:16:27 +0100

    Add project files.

---
+++ Bingoo.DesktopApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Bingoo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Bingoo.DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+namespace Bingoo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddBlazorWebViewDeveloperTools();
+            serviceCollection.AddSpeechSynthesis();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
